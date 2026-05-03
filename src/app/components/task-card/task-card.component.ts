dcc23ef4a90f2acf0e99e87dc7ba2080dcd1058d
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2026-05-03 16:25:10 -0400

    first commit

---
+++ src/app/components/task-card/task-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { ModalControllerService } from '../../services/modal-controller.service';
+
+@Component({
+  selector: 'app-task-card',
+  imports: [],
+  templateUrl: './task-card.component.html',
+  styleUrl: './task-card.component.css',
+})
+export class TaskCardComponent {
+  private readonly _modalControllerService = inject(ModalControllerService);
+
+  openEditTaskModal() {
+    const dialogRef = this._modalControllerService.openEditTaskModal({
+      name: 'Nome da tarefa',
+      description: 'Descrição da tarefa',
+    });
+
+    dialogRef.closed.subscribe((taskForm) => {
+      console.log('Task form data: ', taskForm);
+    });
+  }
+}
